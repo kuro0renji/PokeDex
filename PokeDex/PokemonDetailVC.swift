551dd43042c7948f705d90e58e948bc4340c5410
Hui Lin <lhmida@gmail.com>
2016-11-07 16:20:18 -0600

    Added IBOutlets

--- PokeDex/PokemonDetailVC.swift
+++ PokeDex/PokemonDetailVC.swift
@@ -12,17 +12,58 @@ class PokemonDetailVC: UIViewController {
     
     var pokemon: Pokemon!
     
-    @IBOutlet weak var pokedexID: UILabel!
+    @IBOutlet weak var topPokedexID: UILabel!
     @IBOutlet weak var topNameLbl: UILabel!
-    @IBOutlet weak var centerNameLbl: UILabel!
+    @IBOutlet weak var mainNameLbl: UILabel!
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var DescriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var pokedexIDLbl: UILabel!
+    @IBOutlet weak var baseAttackLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        pokedexID.text = "#\(pokemon.pokedexID)"
-//        topNameLbl.text = pokemon.name.capitalized
-//        centerNameLbl.text = pokemon.name.uppercased()
+        topPokedexID.text = "#\(pokemon.pokedexID)"
+        topNameLbl.text = pokemon.name.capitalized
+        mainNameLbl.text = pokemon.name.uppercased()
         
     }
 
+    @IBAction func backBtn_pressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
